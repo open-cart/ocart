b4b05ca2e4756790ce445f92080dddab4fa0dee9
Phan Trung Nguyên <nguyenpl117@gmail.com>
2021-03-05 15:54:50 +0700

    chore: add template to page

--- system/packages/page/src/Providers/HookServiceProvider.php
+++ system/packages/page/src/Providers/HookServiceProvider.php
@@ -12,6 +12,7 @@ use Illuminate\Support\ServiceProvider;
 use Ocart\Page\Repositories\PageRepository;
 use Ocart\Page\TestMiddleware;
 use Ocart\SeoHelper\Facades\SeoHelper;
+use Ocart\Theme\Facades\Theme;
 
 class HookServiceProvider extends ServiceProvider
 {
@@ -37,6 +38,10 @@ class HookServiceProvider extends ServiceProvider
             $meta->setDescription($page->description);
             $meta->setUrl($page->url);
             $meta->setType('article');
+
+            if ($page->template) {
+                Theme::layout($page->template);
+            }
 
             do_action(BASE_ACTION_PUBLIC_RENDER_SINGLE, PAGE_MODULE_SCREEN_NAME, $page);
 
