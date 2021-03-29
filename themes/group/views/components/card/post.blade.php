78f12182b176a6bacac2eb30b46960a1685c9408
baynb <nguyenbabay96@gmail.com>
2021-03-29 19:45:42 +0700

    sua loi image null post card

--- themes/group/views/components/card/post.blade.php
+++ themes/group/views/components/card/post.blade.php
@@ -2,7 +2,7 @@
 @if($data)
     <a href="/post/{{ $data->id }}" class="h-full block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
         <div class="relative pb-60 overflow-hidden">
-            <img class="absolute inset-0 h-full w-full object-cover" src="{{ TnMedia::url(head($data->images)) }}" alt="{{ $data->name }}">
+            <img class="absolute inset-0 h-full w-full object-cover" src="{{ TnMedia::url($data->image) }}" alt="{{ $data->name }}">
         </div>
         <div class="p-4">
             <h2 class="mt-2 mb-2 font-bold">{{ $data->name }}</h2>
