f313f0cf0827403173971cf1b73efa18843fbe57
baynb <nguyenbabay96@gmail.com>
2021-04-11 01:53:38 +0700

    merge bay_user va bay_cart

--- themes/ripple/routes/web.php
+++ themes/ripple/routes/web.php
@@ -13,4 +13,5 @@ Route::group([
         Route::get('user-profile', function () {
             return Theme::layout('default')->scope('user-profile');
         })->name('user-profile')->middleware(['auth']);
-    });
+    }
+);
