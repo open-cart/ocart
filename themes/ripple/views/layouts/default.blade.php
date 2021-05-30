9df7aacaf6b0a4bbaa6cd5e13a43da9e23d31884
SevenJobs <nguyenbabay96@gmail.com>
2021-05-31 00:04:54 +0700

    them config meta tag

--- themes/ripple/views/layouts/default.blade.php
+++ themes/ripple/views/layouts/default.blade.php
@@ -5,10 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-pjax-version" content="{{ mix('themes/ripple/css/style.css') }}">
 
-    @php
-    $favicon = get_favicon();
-    @endphp
-    <link rel="icon" type="image/png" href="{{ $favicon }}">
+    <link rel="icon" type="image/png" href="{{ get_favicon() }}">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {!! SeoHelper::render() !!}
@@ -34,6 +31,11 @@
             })
         })
     </script>
+
+    <!-- Meta Head -->
+    {!! get_meta_head() !!}
+    <!-- End Meta Head -->
+
 </head>
 <body>
 <div class="font-sans text-gray-900 antialiased">
@@ -181,6 +183,8 @@
         })
     })
 </script>
+
+{!! get_meta_footer() !!}
 
 </body>
 </html>
