385caaaaa8523965d1b94faa6cfee79a3ea7f5ba
SevenJobs <nguyenbabay96@gmail.com>
2021-06-02 00:12:19 +0700

    sua slide

--- themes/ripple/views/index.blade.php
+++ themes/ripple/views/index.blade.php
@@ -1,11 +1,10 @@
 <x-guest-layout xmlns:x-theme="http://www.w3.org/1999/html">
     @php
-        $banner = get_banner();
         $sections = get_config_sections();
         $categories = get_categories();
     @endphp
 
-    @include(Theme::getThemeNamespace('config-section/' . $sections->name . '/sec-slide'))
+    @include(Theme::getThemeNamespace('config/base/sec-slide'))
 
     @if(is_active_plugin('ecommerce') && !empty($categories) && $sections != null && in_array('categories_product', $sections->value))
         <section class="antialiased font-sans pt-16">
@@ -13,7 +12,7 @@
                 <div class="flex flex-wrap -mx-2">
                     @foreach($categories as $category)
                     <div class="w-1/2 sm:w-1/3 md:w-1/4 xl:w-1/6 p-2 hover:shadow-xl text-center">
-                        <a href="{!! route(ROUTE_PRODUCT_CATEGORY_SCREEN_NAME, ['slug'=> $category->slug]) !!}" class="p-2 border border-gray-300 inline-block w-full">
+                        <a href="{!! route(ROUTE_PRODUCT_CATEGORY_SCREEN_NAME, ['slug'=> $category->slug]) !!}" class="p-3 border border-gray-300 inline-block w-full">
                             <img src="{{ TnMedia::url(empty($category->image) ? asset('/images/no-image.jpg') : $category->image) }}" class="w-full block m-auto rounded-full p-2">
                             <div class="text-gray-600 font-bold line-clamp-1">{{ $category->name }}</div>
                         </a>
@@ -24,8 +23,8 @@
         </section>
     @endif
 
-    @if($sections != null && in_array('about', $sections->value))
-        @include(Theme::getThemeNamespace('config-section/' . $sections->name . '/sec-about'))
+    @if(!empty($sections) && in_array('about', $sections->value))
+        @include(Theme::getThemeNamespace('config/' . $sections->name . '/section/sec-about'))
     @endif
 
     @if(is_active_plugin('ecommerce'))
@@ -52,8 +51,8 @@
             </section>
         @endif
     @endif
-    @if($sections != null && in_array('feedback', $sections->value))
-        @include(Theme::getThemeNamespace('config-section/' . $sections->name . '/sec-feedback'))
+    @if(!empty($sections) && in_array('feedback', $sections->value))
+        @include(Theme::getThemeNamespace('config/' . $sections->name . '/section/sec-feedback'))
     @endif
     @if(is_active_plugin('blog'))
         <section class="sec-post antialiased bg-gray-100 font-sans py-16">
@@ -77,15 +76,15 @@
 
         </section>
     @endif
-    @if($sections != null && in_array('partner', $sections->value))
-        @include(Theme::getThemeNamespace('config-section/' . $sections->name . '/sec-partner'))
+    @if(!empty($sections) && in_array('partner', $sections->value))
+        @include(Theme::getThemeNamespace('config/' . $sections->name . '/section/sec-partner'))
     @endif
     @if(is_active_plugin('distributor'))
         @include(Theme::getThemeNamespace('sections.distributor'))
     @endif
 
-    @if(is_active_plugin('contact') && $sections != null && in_array('contact', $sections->value))
-        @include(Theme::getThemeNamespace('config-section/' . $sections->name . '/sec-contact'))
+    @if(is_active_plugin('contact') && !empty($sections) && in_array('contact', $sections->value))
+        @include(Theme::getThemeNamespace('config/' . $sections->name . '/section/sec-contact'))
     @endif
 
 </x-guest-layout>
