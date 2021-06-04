30197b12271dd21555203b1845535ee48bb9fff0
SevenJobs <nguyenbabay96@gmail.com>
2021-06-04 23:39:55 +0700

    ghep config footer

--- themes/ripple/views/config/khithudo/config-home.js
+++ themes/ripple/views/config/khithudo/config-home.js
@@ -65,4 +65,84 @@ const configMenu = {
     ]
 }
 
-console.log(JSON.stringify(configMenu))
+const configMenuFooter = {
+    "data": [
+        {
+            "title": "Sản phẩm",
+            "menu": [
+                {
+                    "name":"Khí công nghiệp",
+                    "slug":"#",
+                },
+                {
+                    "name":"Khí đặc biệt",
+                    "slug":"#",
+                },
+                {
+                    "name":"Khí chuẩn",
+                    "slug":"#",
+                },
+                {
+                    "name":"Khí y tế",
+                    "slug":"#",
+                },
+                {
+                    "name":"Thiết bị ngành khí",
+                    "slug":"#",
+                },
+                {
+                    "name":"Các sản phẩm - dịch vụ khác",
+                    "slug":"#",
+                }
+            ]
+        },
+        {
+            "title": "Tin tức",
+            "menu": [
+                {
+                    "name":"Hoạt động của công ty",
+                    "slug":"/khicongnghiep",
+                },
+                {
+                    "name":"Kiến thức ngành khí",
+                    "slug":"/khicongnghiep",
+                },
+                {
+                    "name":"Ứng dụng khí",
+                    "slug":"#",
+                },
+                {
+                    "name":"Các kiến thức khác",
+                    "slug":"#",
+                },
+            ]
+        },
+        {
+            "title": "Liên kết",
+            "menu": [
+                {
+                    "name": "Trang chủ",
+                    "slug": "#"
+                },
+                {
+                    "name": "Giới thiệu",
+                    "slug": "#"
+                },
+                {
+                    "name": "Liên hệ",
+                    "slug": "#"
+                },
+                {
+                    "name": "Facebook",
+                    "slug": "#"
+                },
+                {
+                    "name": "Zalo",
+                    "slug": "#"
+                },
+            ]
+        }
+    ]
+}
+
+console.log(JSON.stringify(configMenuFooter))
