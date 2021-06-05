f8d75cc1b674d58192aa4bf04410bbef01afbe5d
Phan Trung Nguyên <nguyenpl117@gmail.com>
2021-06-05 11:13:08 +0700

    chore: cập nhật quản lý người dùng.

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -14,7 +14,7 @@ class AdminFactory extends Factory
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Admin::class;
 
     /**
      * Define the model's default state.
