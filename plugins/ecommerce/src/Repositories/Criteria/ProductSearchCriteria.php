d526cae5111e00800d442e4dc592d32d7d96745f
Phan Trung Nguyên <nguyenpl117@gmail.com>
2021-06-12 10:57:27 +0700

    chore: add product search criteria

---
+++ plugins/ecommerce/src/Repositories/Criteria/ProductSearchCriteria.php
@@ -0,0 +1,51 @@
+<?php
+namespace Ocart\Ecommerce\Repositories\Criteria;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Http\Request;
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+class ProductSearchCriteria implements CriteriaInterface
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    /**
+     * Apply criteria in query repository
+     *
+     * @param Builder              $model
+     * @param RepositoryInterface $repository
+     *
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        /** @var Request $request */
+        $request = $this->request;
+
+        if ($request->get('submit') == 'search') {
+            if ($request->get('name')) {
+                $name = $request->get('name');
+                $model = $model->where(function($q) use ($name){
+                    $q->orWhere('name', 'like', "%$name%");
+                    $q->orWhere('description', 'like', "%$name%");
+                });
+            }
+        }
+
+        if ($request->get('order')) {
+
+        } else {
+            $model = $model->orderBy('id', 'desc');
+        }
+
+        return $model;
+    }
+}
