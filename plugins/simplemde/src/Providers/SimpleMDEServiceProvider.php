8e79011af53bf3825c8539030f973f340682d9b8
Phan Trung Nguyên <nguyenpl117@gmail.com>
2021-06-15 19:21:13 +0700

    chore: update simplemde

--- plugins/simplemde/src/Providers/SimpleMDEServiceProvider.php
+++ plugins/simplemde/src/Providers/SimpleMDEServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Ocart\SimpleMDE\Providers;
+use Illuminate\Support\Str;
+use Ocart\Blog\Forms\CategoryForm;
+use Ocart\Blog\Forms\PostForm;
 use Ocart\Core\Forms\Field;
 use Illuminate\Support\ServiceProvider;
 use Ocart\Core\Traits\LoadAndPublishDataTrait;
@@ -9,46 +12,29 @@ class SimpleMDEServiceProvider extends ServiceProvider
 {
     use LoadAndPublishDataTrait;
 
+
+
     public function register()
     {
 
 //        $this->publishAssets(['js']);
+        $that = $this;
 
-        add_filter(BASE_FILTER_BEFORE_RENDER_FORM, function ($a,$b,$c) {
-            if (in_array($b, ['blog_post', 'blog_category'])) {
-                $a->add('content', Field::TEXTAREA, [
-                    'label' => trans('plugins/blog::posts.content'),
-                    'attr' => [
-//            'class' => $this->formHelper->getConfig('defaults.field_class') . ' editor-full'
-                        'id' => 'editor-simplemde'
-                    ]
-                ], true);
+        add_filter(BASE_FILTER_BEFORE_RENDER_FORM, function ($a,$b,$c) use($that) {
+
+            if (method_exists($that, 'addFilterRenderForm' . Str::ucfirst(Str::camel($b)))) {
+                $that->{'addFilterRenderForm' . Str::ucfirst(Str::camel($b))}($a);
             }
+
             return $a;
         },1, 3);
 
-        add_action(BASE_ACTION_META_BOXES, function ($a, $b, $c) {
+        add_action(BASE_ACTION_META_BOXES, function ($a, $b, $c) use($that) {
             if ($b != 'side') {
                 return;
             }
-            echo '
-<script>
-renderSimplemde("editor-simplemde");
-</script>
-<style>
-.CodeMirror .cm-spell-error:not(.cm-url):not(.cm-comment):not(.cm-tag):not(.cm-word) {
-background: none !important;
-}
-@media (prefers-color-scheme: dark) {
-    .editor-toolbar:not(.fullscreen) a {
-        color: white !important;
-    }
-    .editor-toolbar a.active, .editor-toolbar a:hover {
-        color: #2c3e50 !important;
-    }
-}
-</style>
-            ';
+
+            $that->addActionMetaBoxes();
         }, 999, 3);
 
         $scripts = config('assets.scripts');
@@ -72,5 +58,47 @@ background: none !important;
         config()->set('assets.resources.scripts', $resourceScript);
         config()->set('assets.scripts', $scripts);
         config()->set('assets.styles', $styles);
+    }
+
+    public function addFilterRenderFormBlogPost(PostForm $form)
+    {
+        $form->add('description', Field::TEXTAREA, [
+            'label'      => trans('plugins/blog::categories.description'),
+            'attr' => [
+                'id' => 'editor-simplemde'
+            ]
+        ], true);
+    }
+
+    public function addFilterRenderFormBlogCategory(CategoryForm $form)
+    {
+        $form->add('content', Field::TEXTAREA, [
+            'label' => trans('plugins/blog::posts.content'),
+            'attr' => [
+                'id' => 'editor-simplemde'
+            ]
+        ], true);
+    }
+
+    public function addActionMetaBoxes()
+    {
+        echo '
+<script>
+renderSimplemde("editor-simplemde");
+</script>
+<style>
+.CodeMirror .cm-spell-error:not(.cm-url):not(.cm-comment):not(.cm-tag):not(.cm-word) {
+background: none !important;
+}
+@media (prefers-color-scheme: dark) {
+    .editor-toolbar:not(.fullscreen) a {
+        color: white !important;
+    }
+    .editor-toolbar a.active, .editor-toolbar a:hover {
+        color: #2c3e50 !important;
+    }
+}
+</style>
+            ';
     }
 }
