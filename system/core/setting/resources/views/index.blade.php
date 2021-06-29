4deb6516b11ea4abaaa8df1dec079b5cabc746a0
Phan Trung Nguyên <nguyenpl117@gmail.com>
2021-06-29 23:31:22 +0700

    chore: update setting options

--- system/core/setting/resources/views/index.blade.php
+++ system/core/setting/resources/views/index.blade.php
@@ -3,13 +3,13 @@
     <div class="py-12 pb-28 max-w-screen-lg mx-auto">
         <div class="sm:px-6 lg:px-8 space-y-8">
             <div class="grid grid-cols-12 gap-4">
-                <div class="col-span-3 pt-6">
+                <div class="col-span-4 pt-6">
                     <h3 class="text-2xl">
                         {{ trans('core/setting::setting.general.general_block') }}
                     </h3>
                     <p>{{ trans('core/setting::setting.general.description') }}</p>
                 </div>
-                <div class="col-span-9 space-y-4">
+                <div class="col-span-8 space-y-4">
                     <div class="bg-white border dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 border-b border-gray-200 space-y-4">
                             <div class="flex flex-col">
@@ -20,27 +20,19 @@
                                          value="{{ setting('admin_email') }}"
                                          name="admin_email"></x-input>
                             </div>
-{{--                            <div class="flex flex-col">--}}
-{{--                                <label for="time_zone">{{ trans('core/setting::setting.general.time_zone') }}</label>--}}
-{{--                                <x-input id="time_zone" name="time_zone" value="{{ setting('time_zone') }}"/>--}}
-{{--                            </div>--}}
-{{--                            <div class="flex flex-col">--}}
-{{--                                <label for="time_zone">{{ trans('core/setting::setting.general.time_zone') }}</label>--}}
-{{--                                <x-input id="time_zone" name="time_zone" value="{{ setting('time_zone') }}"/>--}}
-{{--                            </div>--}}
                         </div>
                     </div>
                 </div>
             </div>
             <div><hr></div>
             <div class="grid grid-cols-12 gap-4">
-                <div class="col-span-3 pt-6">
+                <div class="col-span-4 pt-6">
                     <h3 class="text-2xl">
                         {{ trans('core/setting::setting.general.admin_appearance_title') }}
                     </h3>
                     <p>{{ trans('core/setting::setting.general.admin_appearance_description') }}</p>
                 </div>
-                <div class="col-span-9 space-y-4">
+                <div class="col-span-8 space-y-4">
                     <div class="bg-white border dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 border-b border-gray-200 space-y-4">
                             <div class="flex flex-col">
--- system/core/setting/routes/web.php
+++ system/core/setting/routes/web.php
@@ -11,19 +11,19 @@ Route::group(
         Route::group([
             'prefix' => ADMIN_PREFIX,
         ], function() {
+            Route::get('general', [
+                'as'         => 'settings.options',
+                'uses'       => 'SettingController@getOptions',
+                'permission' => 'settings.options',
+            ]);
+
+            Route::post('general/edit', [
+                'as'         => 'settings.edit',
+                'uses'       => 'SettingController@postEdit',
+                'permission' => 'settings.options',
+            ]);
+
             Route::group(['prefix' => 'email', 'permission' => 'settings.email'], function () {
-                Route::get('general', [
-                    'as'         => 'settings.options',
-                    'uses'       => 'SettingController@getOptions',
-                    'permission' => 'settings.options',
-                ]);
-
-                Route::post('general/edit', [
-                    'as'         => 'settings.edit',
-                    'uses'       => 'SettingController@postEdit',
-                    'permission' => 'settings.options',
-                ]);
-
                 Route::get('', [
                     'as'   => 'settings.email',
                     'uses' => 'SettingController@getEmailConfig',
