a0db541cfe64d03ed5c51acb4b6393946b0e3c4a
Phan Trung Nguyên <nguyenpl117@gmail.com>
2021-07-22 09:48:30 +0700

    chore: update table link

--- system/core/table/resources/views/simple-table.blade.php
+++ system/core/table/resources/views/simple-table.blade.php
@@ -42,7 +42,7 @@
                         {!! $table->table(['class' => 'w-full']) !!}
                     </div>
                     <div class="mt-2">
-                        {!! method_exists($table->data, 'link') && $table->data->links() !!}
+                        {!! is_object($table->data) && method_exists($table->data, 'link') && $table->data->links() !!}
                     </div>
                 </div>
             </div>
