a7f7a6fdb8f7b175ea9692a6e5a043feaad5e1a5
SevenJobs <nguyenbabay96@gmail.com>
2022-06-24 20:56:49 +0700

    page smart connect

--- themes/vanchuyen/views/page.blade.php
+++ themes/vanchuyen/views/page.blade.php
@@ -1,5 +1,17 @@
 <x-guest-layout>
-    <div class="section-custom container-custom content-mce">
-        {!! $page->content !!}
+    <div>
+        <div class="py-12" style="background: #00a5ff">
+            <div class="container-custom">
+                <div class="text-white text-xl lg:text-3xl font-bold">
+                    {{ $page->name }}
+                </div>
+                <div class="text-white">
+                    {{ $page->description }}
+                </div>
+            </div>
+        </div>
+        <div class="section-custom container-custom content-mce">
+            {!! $page->content !!}
+        </div>
     </div>
 </x-guest-layout>
